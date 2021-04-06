bf6e7db978588a74d3b2469b4a8187624fc20ee0
Arminas Miliauskis <s032675@ad.viko.lt>
2021-04-06 22:40:57 +0300

    Implemented a File class

---
+++ RSA_Algorithm/File.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace RSA_Algorithm
+{
+    class File
+    {
+        private string text;
+
+        public void WriteFile(string text, string path)
+        {
+            if (!System.IO.File.Exists(path))
+            {
+                System.IO.File.Create(path);
+                TextWriter tw = new StreamWriter(path);
+                tw.WriteLine(text);
+                tw.Close();
+            }
+            else if (System.IO.File.Exists(path))
+            {
+                TextWriter tw = new StreamWriter(path);
+                tw.WriteLine(text);
+                tw.Close();
+            }
+        }
+
+        public string ReadFile(string path)
+        {
+            if (System.IO.File.Exists(path))
+            {
+                string textas = "";
+                using (StreamReader sr = System.IO.File.OpenText(path))
+                {
+                    string s = "";
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        textas += s;
+                    }
+                }
+                this.text = textas;
+            }
+
+            return text;
+        }
+    }
+}
--- RSA_Algorithm/Form1.cs
+++ RSA_Algorithm/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.IO;
 namespace RSA_Algorithm
 {
     public partial class Form1 : Form
@@ -26,12 +26,9 @@ namespace RSA_Algorithm
             int N = rsa.getN(Convert.ToInt32(tb_pValue.Text), Convert.ToInt32(tb_qValue.Text)); // part of public key
             int fi = rsa.GetFi(Convert.ToInt32(tb_pValue.Text), Convert.ToInt32(tb_qValue.Text));
             //int E = rsa.GetE(rsa.GetFi(Convert.ToInt32(tb_pValue.Text), Convert.ToInt32(tb_qValue.Text)));   // part of public key           
-            int E = rsa.GetE(fi);
-            //RSA.Cipher(tb_Text.ToString(), N, E);
+            int E = rsa.GetE(fi);         // part of public key   
             int d = rsa.computeD(E, fi);
             tb_Text.Text = RSA.Cipher(tb_Text.ToString(), N, E);
-
-
         }
 
         private void bt_Decrypt_Click(object sender, EventArgs e)
